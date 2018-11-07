1e01507274b01b9089f4d951512103736db27deb
mathieuway91 <mathieu.way91@gmail.com>
2018-11-07 09:54:47 +0100

    nettoyage du script de lobby
    TODO: -Besoin d'optimisation

--- Platunum-ProjectU/Assets/scripts/Debug Mathieu/LobbyManager.cs
+++ Platunum-ProjectU/Assets/scripts/Debug Mathieu/LobbyManager.cs
@@ -43,51 +43,53 @@ namespace Manager
             {
                 if(Input.anyKey)
                 {
-                    System.Array values = System.Enum.GetValues(typeof(KeyCode));
+                    System.Array values = System.Enum.GetValues(typeof(KeyCode)); //Récupère la liste des touch dispo
                     foreach (KeyCode code in values)
                     {
                         string lastKeyDown = code.ToString();
+                        //Si c'est un joystick
                         if (lastKeyDown.Contains("Joystick"))
                         {
+                            //Rècupère l'id du joystick
                             char c_ControllerId = lastKeyDown.Substring(8, 1)[0];
+                            //Récupère la touch detecté
                             string button = lastKeyDown.Substring(9);
+
+                            //Si c'est un controleur unique
                             if (char.IsDigit(c_ControllerId))
                             {
+                                //Converti l'id (String -> Int)
                                 int i_ControllerId = (int)char.GetNumericValue(c_ControllerId);
-                                if (Input.GetKey(code)){
-                                    print(System.Enum.GetName(typeof(KeyCode), code));
-                                    //Debug.Log("Joystick n°:" + ControllerId);
-                                    //bool PlayerAlreadyJoin = PlayerList.Exists(item => item.ControllerId == ControllerId);
-                                    if (!playerManager.IsPlayerAlreadyInLobby(i_ControllerId) && button=="Button0")
+                                //Si l'input est appuyer
+                                if(Input.GetKey(code)){
+                                    //print(System.Enum.GetName(typeof(KeyCode), code));
+                                    if (!playerManager.IsPlayerAlreadyInLobby(i_ControllerId) && button == "Button0")
                                     {
-                                        int playerId = playerManager.AddPlayer(i_ControllerId);
-                                        Player player = playerManager.GetPlayer(playerId);
-                                        ReadyPlayerList.Add(playerId, false);
+
+                                        Player player = playerManager.AddPlayer(i_ControllerId);
+                                        ReadyPlayerList.Add(player.id, false);
+
                                         //Update PlayerUI
                                         Sprite sprite = player.Class.icon;
-                                        PlayerUI[playerId - 1].Find("Sprite").GetComponent<Image>().sprite = sprite;
-                                        PlayerUI[playerId - 1].gameObject.SetActive(true);
+                                        PlayerUI[player.id - 1].Find("Sprite").GetComponent<Image>().sprite = sprite;
+                                        PlayerUI[player.id - 1].Find("Sprite").GetComponent<Image>().preserveAspect = true;
+                                        PlayerUI[player.id - 1].gameObject.SetActive(true);
                                     }
                                     else
                                     {
-                                        //Debug.Log("Already joined");
+
                                         if (Input.GetKeyDown(code))
                                         {
                                             if (button == "Button4" || button == "Button5")
                                             {
+                                                //Le joueur de se controleur souhaite changer de class
                                                 Player player = playerManager.GetPlayer(playerManager.GetPlayerByControllerId(i_ControllerId));
+                                                //récupère la classes du joueur
                                                 int CurrentClass = classManager.GetClassIdByName(player.Class.name);
-                                                /*int newclass = CurrentClass;
-                                                int increment;
-                                                if (button == "Button5")
-                                                    increment = 1;
-                                                else
-                                                {
-                                                    if(CurrentClass == 0)
 
-                                                }
-                                                newclass = (newclass + increment) % classManager.GetClassCount();*/
-                                                int newclass = CurrentClass;
+                                                int newclass = CurrentClass;//la variable newclass déterminera la prochaine classes
+
+                                                // Gère les problèmes de tableau pour evité le out of range
                                                 if (button == "Button5")
                                                 {
                                                     newclass = (CurrentClass +1) % classManager.GetClassCount();
@@ -100,22 +102,24 @@ namespace Manager
                                                         newclass = classManager.GetClassCount() -1;
 
                                                 }
+                                                //ajoute la nouvelle classe au joueur
                                                 player.Class = classManager.GetClassById(newclass);
+                                                //UI Update
                                                 PlayerUI[player.id - 1].Find("Sprite").GetComponent<Image>().sprite = player.Class.icon;
                                             }
                                         }
-                                        
-
                                         if(button == "Button1")
                                         {
-                                            int PlayerId = playerManager.GetPlayerByControllerId(i_ControllerId);
+                                            //Le joueur de ce controlleur est ready
+                                            int PlayerId = playerManager.GetPlayerByControllerId(i_ControllerId); //recupère l'id
+                                            //Change la valeur ready
                                             ChangeReadyState(PlayerId, true);
                                         }
                                     }
                                 }
-                                //
                                 else
                                 {
+                                    //Si la touche pour etre ready n'est pas maintenu -> change la valeur ready du joueur to false
                                     if (button == "Button1" && playerManager.IsPlayerAlreadyInLobby(i_ControllerId))
                                     {
                                         int PlayerId = playerManager.GetPlayerByControllerId(i_ControllerId);
@@ -128,62 +132,64 @@ namespace Manager
                 }
                 else
                 {
-                    //Debug.Log("Aucun Joueur n'est pret "+ReadyPlayerList.Count);
+                    //Aucun input detecté (les joueurs ne m'entienne pas la touche ready
+                    //Change tous les joueurs en non ready
                     for (int i = 1; i < ReadyPlayerList.Count+1; i++)
                     {
                         ChangeReadyState(i, false);
                     }
                 }
+                //Check si tous les joueurs sont ready
                 CheckReadyPlayer();
             }
-        }
-
-        private void ChangePlayer()
-        {
-
         }
 
         private void CheckReadyPlayer()
         {
             bool flag = true;
+            //Check Si tous les joueurs sont ready
             foreach (KeyValuePair<int,bool> PlayerReadyState in ReadyPlayerList)
             {
                 if (!PlayerReadyState.Value)
                     flag = false;
             }
-            if (flag && ReadyPlayerList.    Count > 0)
+            //Temps total avec tous les joueurs ready
+            if (flag && ReadyPlayerList.Count > 0)
                 timer += Time.deltaTime;
             else
                 timer = 0;
-
+            //Si temps total des joueurs ready atteint lancé le niveau
             if (timer >= ReadyTimeHold)
             {
                 StartGame();
             }
         }
 
+        //Fonction pour change la valeur de ready d'un joueur selon son ID
         private void ChangeReadyState(int PlayerId, bool state)
         {
-            //Debug.Log("Player n°:" + PlayerId + (state ? "ready" : "not ready"));
             if (PlayerId > 0)
             {
-                //Debug.Log("Player n°:" + PlayerId + (state ? "ready" : "not ready"));
                 PlayerUI[PlayerId - 1].Find("StateText").gameObject.SetActive(state);
                 ReadyPlayerList[PlayerId] = state;
             }
         }
 
+        //Lorsque tous les joueurs sont ready
+        //Charge la nouvelle scène
         private void StartGame()
         {
             SceneManager.LoadScene(SceneToLoad);
             StartGameBool = true;
         }
 
+        //déclenche le chargement du niveau
         private void onSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             if (StartGameBool)
             {
                 playerManager.LoadScene();
+                Destroy(this.gameObject);
             }
         }
     }
