f0185c88ab86d1ebaf05fcae5ecfbea36730cdf9
paul thibault <paulthb1@gmail.com>
2018-11-07 17:11:50 +0100

    healthBar
    script et ui healthbar dans la scene de paul

---
+++ Platunum-ProjectU/Assets/Scripts/Paul Script/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    private Image healthbar;
+    [SerializeField]
+    private Text rationText;
+
+    private float HitPoint = 150f;
+    private float MaxHitPoint = 150f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //pour les tests
+        if (Input.GetKeyDown("space"))
+        {
+            TakeDamage();
+        }
+
+    }
+
+    private void UpdateHeatlBar()
+    {
+        float ratio = HitPoint / MaxHitPoint;
+        healthbar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        rationText.text = (ratio * 100).ToString() + '%'; 
+    }
+
+    private void TakeDamage()
+    {
+        HitPoint -= 10;
+        if (HitPoint < 0)
+        {
+            HitPoint = 0;
+            Debug.Log("on est mort !");
+        }
+
+        UpdateHeatlBar();
+    }
+
+    private void TakeArmor()
+    {
+    }
+}
