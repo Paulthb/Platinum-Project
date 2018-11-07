8e4ae4cfe3b3474e6ce07673ffd39ff7a62aad36
JeanDev <hardyjea@gmail.com>
2018-11-07 08:29:22 +0100

    Début tests partition

--- Platunum-ProjectU/Assets/Scripts/Activator.cs
+++ Platunum-ProjectU/Assets/Scripts/Activator.cs
@@ -4,32 +4,55 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour {
 
+    SpriteRenderer sr;
     public KeyCode key;
     bool active = false;
     GameObject note;
+    Color old;
 
-	// Use this for initialization
-	void Start () {
-		
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    
+    void Start () {
+        old = sr.color;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+            StartCoroutine(Pressed());
+
         if (Input.GetKeyDown(key) && active)
-        {
+        { 
             Destroy(note);
+            AddScore();
+            active = false;
         }
 	}
 
-    private void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         active = true;
         if (col.gameObject.tag == "Note")
             note = col.gameObject;
     }
 
-    private void OnTriggerExit2D(Collider2D col)
+    void OnTriggerExit2D(Collider2D col)
     {
         active = false;
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
+    }
+
+    IEnumerator Pressed()
+    {
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
     }
 }
