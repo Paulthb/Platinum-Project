48a9830b280c826e448b84ce057acbb44e19dd6b
paul thibault <paulthb1@gmail.com>
2018-11-09 12:07:20 +0100

    Script & prefab enemy, mana et harmonie bar
    harmonie still in progress

---
+++ Platunum-ProjectU/Assets/Scripts/Paul Script/HarmonieBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HarmonieBar : MonoBehaviour {
+
+    [SerializeField]
+    private Image harmonieBar;
+
+    private float harmoniePoint = 0f;
+    private float harmonieMaxPoint = 100f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        UpdateBar();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //pour les tests
+        if (Input.GetKeyDown("x"))
+        {
+            TakeHarmonie();
+        }
+
+    }
+
+    private void TakeHarmonie()
+    {
+
+        harmoniePoint += 10;
+        if (harmoniePoint >= 100)
+        {
+            harmoniePoint = 100;
+            Debug.Log("on passe en fois 2 !");
+        }
+
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        float ratio;
+        ratio = harmoniePoint / harmonieMaxPoint;
+        harmonieBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+}
---
+++ Platunum-ProjectU/Assets/Scripts/Paul Script/ManaBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour {
+
+    [SerializeField]
+    private Image manaBar;
+
+    private float manaPoint = 100f;
+    private float manaMaxPoint = 100f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //pour les tests
+        if (Input.GetKeyDown("c"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void UpdateBar()
+    {
+        float ratio;
+        ratio = manaPoint / manaMaxPoint;
+        manaBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void TakeDamage()
+    {
+
+        manaPoint -= 10;
+        if (manaPoint < 0)
+        {
+            manaPoint = 0;
+            Debug.Log("on est mort !");
+        }
+
+        UpdateBar();
+    }
+}
