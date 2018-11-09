9928d52453b2da49ecd58f35352552ebc8a1cd99
mathieuway91 <mathieu.way91@gmail.com>
2018-11-09 12:22:12 +0100

    Nouveau System de Synchro

---
+++ Platunum-ProjectU/Assets/Scripts/Debug Mathieu/Sync song/Game Control/inputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputManager : MonoBehaviour {
+
+    //singleton
+    public static inputManager instance;
+    public KeyCodePlayer[] keyCodePlayer;
+    public KeyCode GetKeyCode(int partitionId, int i)
+    {
+        return keyCodePlayer[partitionId].keys[i];
+    }
+
+    void Awake()
+    {
+        //singleton
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    [System.Serializable]
+    public class KeyCodePlayer
+    {
+        public KeyCode[] keys;
+    }
+}
