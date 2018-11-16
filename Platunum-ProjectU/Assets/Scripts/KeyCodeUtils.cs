7c854a69b67cdcc7cab9564758439c4bed75981c
mathieuway91 <mathieu.way91@gmail.com>
2018-11-16 11:19:08 +0100

    Ajout KeyCodeUtils

---
+++ Platunum-ProjectU/Assets/Scripts/KeyCodeUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCode {
+
+	public static KeyCodeUtils GetKeyCode(string KeyString)
+    {
+        return (KeyCode)System.Enum.Parse(typeof(KeyCode), KeyString);
+    }
+}
