905e22f4abd2dfd61ede6f72f0a6c65496ab6d53
JeanDev <hardyjea@gmail.com>
2018-12-17 19:08:12 +0100

    Creation soundmanager

---
+++ Platunum-ProjectU/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume = 0.6f;
+    [Range(0.5f, 1.5f)]
+    public float pitch = 1f;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+    }
+}
+
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    Sound[] sounds;
+    
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        // no sound with _name
+        Debug.LogWarning("AudioManager: Sound not found in list: " + _name);
+    }
+}
